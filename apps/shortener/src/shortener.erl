c7e76c789860ce7053a3b8aabc3ad6e808720738
Andrey Mayorov <a.mayorov@rbkmoney.com>
2017-12-14 12:11:51 +0300

    MSPF-320: Splice supervisor and app entrypoint into a single file for simplicity

--- apps/shortener/src/shortener.erl
+++ apps/shortener/src/shortener.erl
@@ -6,15 +6,42 @@
 -export([start/2]).
 -export([stop /1]).
 
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
 %%
 
 -spec start(normal, any()) -> {ok, pid()} | {error, any()}.
 
 start(_StartType, _StartArgs) ->
-    shortener_sup:start_link().
+    ?MODULE:start_link().
 
 -spec stop(any()) -> ok.
 
 stop(_State) ->
     ok.
 
+%% API
+
+-spec start_link() -> {ok, pid()} | {error, {already_started, pid()}}.
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% Supervisor callbacks
+
+-spec init([]) -> {ok, {supervisor:sup_flags(), [supervisor:child_spec()]}}.
+
+init([]) ->
+    Authorizers = genlib_app:env(shortener, authorizers, #{}),
+    AuthorizerSpec = shortener_authorizer_jwt:get_child_spec(maps:get(jwt, Authorizers)),
+    SwaggerServerSpec = shortener_swagger_server:child_spec(shortener_handler),
+    {ok, {
+       {one_for_all, 0, 1},
+       [AuthorizerSpec, SwaggerServerSpec]
+    }}.
