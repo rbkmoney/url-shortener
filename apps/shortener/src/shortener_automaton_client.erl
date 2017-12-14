0c43086db8f6e871caebee58eca84e041a623c6e
Andrey Mayorov <a.mayorov@rbkmoney.com>
2017-12-14 12:45:27 +0300

    MSPF-320: Refactor and modernize automaton client

--- apps/shortener/src/shortener_automaton_client.erl
+++ apps/shortener/src/shortener_automaton_client.erl
@@ -2,7 +2,6 @@
 -include_lib("mg_proto/include/mg_proto_state_processing_thrift.hrl").
 
 -export([call/4]).
--export([call/5]).
 -export([get_history/3]).
 -export([get_history/4]).
 -export([start/3]).
@@ -22,68 +21,51 @@
 %%
 
 -spec call(ns(), id(), args(), context()) ->
-    response() |
-    no_return().
+    {ok, response()} | {error, notfound} | no_return().
 call(NS, ID, Args, Context) ->
     call(NS, ID, #'mg_stateproc_HistoryRange'{}, Args, Context).
 
--spec call(ns(), id(), history_range(), args(), context()) ->
-    response() |
-    no_return().
 call(NS, ID, HistoryRange, Args, Context) ->
     Descriptor = construct_descriptor(NS, ID, HistoryRange),
     case issue_rpc('Call', [Descriptor, Args], Context) of
         {ok, Result} ->
-            Result;
+            {ok, Result};
         {error, #'mg_stateproc_MachineNotFound'{}} ->
-            ok = start(NS, ID, Context),
-            call(NS, ID, Args, Context)
+            {error, notfound}
     end.
 
 -spec get_history(ns(), id(), context()) ->
-    {ok, history()} |
-    {error,
-        mg_proto_state_processing_thrift:'EventNotFound'() |
-        mg_proto_state_processing_thrift:'MachineNotFound'()
-    } |
-    no_return().
+    {ok, history()} | {error, notfound} | no_return().
 get_history(NS, ID, Context) ->
     get_history(NS, ID, #'mg_stateproc_HistoryRange'{}, Context).
 
--spec get_history(ns(), id(), history_range(), context()) ->
-    {ok, history()} |
-    {error,
-        mg_proto_state_processing_thrift:'EventNotFound'() |
-        mg_proto_state_processing_thrift:'MachineNotFound'()
-    } |
-    no_return().
 get_history(NS, ID, HistoryRange, Context) ->
     Descriptor = construct_descriptor(NS, ID, HistoryRange),
     case issue_rpc('GetMachine', [Descriptor], Context) of
         {ok, #'mg_stateproc_Machine'{history = History}} ->
             {ok, History};
-        {error, _} = Error ->
-            Error
+        {error, #mg_stateproc_MachineNotFound{}} ->
+            {error, notfound}
     end.
 
 -spec start(ns(), id(), context()) ->
-    ok | no_return().
+    ok | {error, exists} | no_return().
 start(NS, ID, Context) ->
     case issue_rpc('Start', [NS, ID, {nl, #mg_msgpack_Nil{}}], Context) of
         {ok, _} ->
             ok;
         {error, #'mg_stateproc_MachineAlreadyExists'{}} ->
-            ok
+            {error, exists}
     end.
 
 -spec remove(ns(), id(), context()) ->
-    ok | no_return().
+    ok | {error, notfound} | no_return().
 remove(NS, ID, Context) ->
     case issue_rpc('Remove', [NS, ID], Context) of
         {ok, _} ->
             ok;
-        {error, #'mg_stateproc_MachineAlreadyExists'{}} ->
-            ok
+        {error, #mg_stateproc_MachineNotFound{}} ->
+            {error, notfound}
     end.
 
 -spec construct_descriptor(ns(), id(), history_range()) ->
@@ -99,14 +81,9 @@ construct_descriptor(NS, ID, HistoryRange) ->
     term() | no_return().
 issue_rpc(Method, Args, Context) ->
     Request = {{mg_proto_state_processing_thrift, 'Automaton'}, Method, Args},
-    {ok, URL} = application:get_env(shortener, automaton_service_url),
-    Opts = #{
-        url => URL,
-        transport_opts => [
-            {recv_timeout, 60000}
-        ]
-    },
-    case woody_client:call(Request, Opts, Context) of
+    ClientOpts0 = maps:get(automaton, genlib_app:env(shortener, service_clients)),
+    ClientOpts1 = ClientOpts0#{event_handler => scoper_woody_event_handler},
+    case woody_client:call(Request, ClientOpts1, Context) of
         {ok, _} = Ok ->
             Ok;
         {exception, #'mg_stateproc_NamespaceNotFound'{}} ->
